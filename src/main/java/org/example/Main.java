a493ef9fa1c22807283a7604a7dedd198f093d98
KamilD1 <145699399+KamilD1@users.noreply.github.com>
2023-11-13 23:18:53 +0100

    Add first code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kamil\\Desktop\\sda Tester\\java\\\\saucedemo\\Chromedriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver= new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+
     }
 }
