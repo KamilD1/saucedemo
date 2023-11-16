612922e119dc0c899fc0f0339733f3eef5c76a83
KamilD1 <145699399+KamilD1@users.noreply.github.com>
2023-11-16 11:41:41 +0100

    add new class

---
+++ src/main/java/org/example/ProductTable.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductTable {
+    WebDriver driver;
+    public ProductTable(WebDriver driver){
+        this.driver = driver;
+    }
+    public void SearchProduct(){
+
+    }
+
+
+}
